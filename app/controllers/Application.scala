5245497b0fc7b2898412b863dfdae1cde4f6e47e
Kentaro Maeda <many.front@gmail.com>
2013-03-27 17:43:57 +0900

    とりあえず引数ありのルーティング追加してみた

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,14 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
+    
     Ok(views.html.index("Your new application is ready."))
   }
   
+  def list(name:String) = Action {
+  	val list = List(name, name, name, name)
+  	
+  	Ok(views.html.list(list))
+  }
+
 }
