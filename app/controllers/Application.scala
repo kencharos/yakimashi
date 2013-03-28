a16c326f6ca0d5b95aedf69c2b71a29e1128439b
Kentaro Maeda <many.front@gmail.com>
2013-03-28 15:48:41 +0900

    画像一覧表示まで

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import model._
+import java.io.File;
 
 object Application extends Controller {
   
@@ -10,10 +12,17 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
   
-  def list(name:String) = Action {
-  	val list = List(name, name, name, name)
-  	
-  	Ok(views.html.list(list))
-  }
+  def albums = Action {
+  	val files = new File("public/album").listFiles();
 
+  	Ok(views.html.albums(files.filter(_ isDirectory).map(_ getName)));
+    
+  }
+  
+  
+  def photo(name:String) = Action {
+  	var images = new File("public/album", name).listFiles().filter(_ isFile)
+  	val photos = images.map(f => Photo("album/" + name + "/" + f.getName, f.getName))
+	Ok(views.html.photo(name, photos))
+  }
 }
---
+++ app/model/Models.scala
@@ -0,0 +1,3 @@
+package model
+
+case class Photo(url:String, name:String)
