230f48543971bb4519270ef05d723f9b1dbf8b68
kencharos <many.front@gmail.com>
2013-03-29 22:51:26 +0900

    mongodb seting

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,13 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
-	
+  		"se.radley" %% "play-plugins-salat" % "1.2"
+		)
+
     val main = play.Project(appName, appVersion, appDependencies).settings(
-      // Add your own project settings here      
+      routesImport += "se.radley.plugin.salat.Binders._",
+      templatesImport += "org.bson.types.ObjectId",
+      resolvers += Resolver.sonatypeRepo("snapshots")
     )
 
 }
