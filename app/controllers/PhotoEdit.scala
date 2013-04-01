6bcaf5cb3be1d22b485f03a7061a6c9555423553
kencharos <many.front@gmail.com>
2013-04-01 23:46:06 +0900

    スライドショー、モーダル追加

---
+++ app/controllers/PhotoEdit.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import model._
+
+object PhotoEdit extends Controller {
+  def info(album:String, name:String) = TODO
+  def update = TODO
+}
