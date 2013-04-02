242f57ff3a704928ace6da854bbe277df815c7c9
kencharos <many.front@gmail.com>
2013-04-02 22:58:24 +0900

    微調整

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -10,18 +10,6 @@ import se.radley.plugin.salat._
 import com.mongodb.casbah.commons.MongoDBObject
 import mongoContext._
 
-case class Photo(@Key("_id")id:ObjectId = new ObjectId, 
-	url:String, 
-	name:String, 
-	labelkeys:Seq[String] = Seq(), 
-	etc:Int = 0,
-	comment:String = "") {
-
-	def count = labelkeys.size + etc
-}
-
-
-case class Label(@Key("_id")id:String, name:String);
 
 object Label extends ModelCompanion[Label, String] {
   val dao = new SalatDAO[Label, String](collection = mongoCollection("label")) {}
@@ -29,3 +17,20 @@ object Label extends ModelCompanion[Label, String] {
   def updateName(label:Label) = dao.update(MongoDBObject("_id" -> label.id),
       MongoDBObject("name" -> label.name), false, false)
 }
+
+object Photo extends ModelCompanion[Photo, ObjectId] {
+  val dao = new SalatDAO[Photo, ObjectId](collection = mongoCollection("photo")) {}
+}
+
+case class Photo(@Key("_id")id:ObjectId = new ObjectId,
+	url:String,
+	name:String,
+	labelkeys:Seq[String] = Seq(),
+	etc:Int = 0,
+	comment:String = "",
+	noDisp:Boolean = false) {
+
+	def count = labelkeys.size + etc
+}
+
+case class Label(@Key("_id")id:String, name:String);
